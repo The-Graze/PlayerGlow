11e56a58dc11dd072e7f45e092db7633ce8ae782
Graze <82724623+The-Graze@users.noreply.github.com>
2025-05-31 23:00:54 +0100

    Add project files.

---
+++ PlayerGlow/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace PlayerGlow
+{
+    [BepInDependency("com.brokenstone.gorillatag.phosphorite")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(InitBruv);
+        }
+
+        private void InitBruv()
+        {
+            var baseLight = GorillaTagger.Instance.mainCamera.GetComponentInChildren<GameLight>(includeInactive: true);
+            foreach (VRRig rig in Resources.FindObjectsOfTypeAll<VRRig>())
+            {
+                GameLight PlayerLight = Instantiate(baseLight);
+                GameLightingManager.instance.AddGameLight(PlayerLight);
+                PlayerLight.AddComponent<PlayerLightHandler>();
+                PlayerLight.transform.SetParent(rig.transform, false);
+                PlayerLight.transform.localPosition = Vector3.zero;
+                FindObjectOfType<BetterDayNightManager>().OnEnable();
+            }
+        }
+    }
+    class PlayerLightHandler : MonoBehaviour
+    {
+        VRRig rig;
+        GameLight light;
+
+        void OnEnable()
+        {
+            rig = transform.parent.GetComponent<VRRig>();
+            light = GetComponent<GameLight>();
+            name = "PlayerLight";
+            GameLightingManager.instance.AddGameLight(light);
+        }
+
+        void FixedUpdate()
+        {
+            if (rig && light)
+            {
+                light.light.color = rig.playerColor;
+            }
+            else
+            {
+                OnEnable();
+            }
+        }
+    }
+}
---
+++ PlayerGlow/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayerGlow
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.playerglow";
+        public const string Name = "PlayerGlow";
+        public const string Version = "1.0.0";
+    }
+}
